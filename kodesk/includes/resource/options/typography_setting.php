aa86a063723b024759ca8636ea4d9641a38cc2d9
DavidFleenor <130915007+DavidFleenor@users.noreply.github.com>
2023-07-07 14:44:37 +0200

    delete unnecessary files

--- kodesk/includes/resource/options/typography_setting.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-
-    'title'         => esc_html__( 'Typography Settings', 'kodesk' ),
-    'id'            => 'typography_setting',
-    'desc'          => '',
-    'icon'          => 'el el-edit'
-);
